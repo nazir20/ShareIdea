c5b67ae4f965c6a68cdba6bb89c74cb090f718e3
Mohammad Nazir Sharifi <nazirsharifi19@gmail.com>
2023-08-10 22:36:19 +0300

    ShareIdea App initial commit

---
+++ src/main/java/com/example/ShareIdea/ShareIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShareIdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareIdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShareIdea/ShareIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShareIdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
